f2851a19384790201ce7ea8987f3d05a8b5fe836
Gal Shnapp <gal.shnapp@gmail.com>
2023-05-11 22:50:07 +0300

    canvas export to mathpix

--- notebook/src/canvas/Canvas.js
+++ notebook/src/canvas/Canvas.js
@@ -1,5 +1,21 @@
 import * as React from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
+import axios from "axios";
+
+
+const base64ToLatex = (base64) => {
+    const headers = { 
+        'app_id': 'gal_shnapp_gmail_com_9439c6_c2248b',
+        'app_key': 'b7cc65bbffe436311a2e2338e1234e07e549b191eae2d2d2af9a5c7cabcd3c25',
+        'Content-type': 'application/json'
+    };
+
+    const data = {
+        'url': base64
+    }
+    axios.post('https://api.mathpix.com/v3/latex', data, { headers })
+        .then(response => console.log(response));
+}
 
 const Canvas = class extends React.Component {
   constructor(props) {
@@ -21,7 +37,7 @@ const Canvas = class extends React.Component {
             this.canvas.current
               .exportImage("jpeg")
               .then((data) => {
-                console.log(data);
+                base64ToLatex(data);
               })
               .catch((e) => {
                 console.log(e);
